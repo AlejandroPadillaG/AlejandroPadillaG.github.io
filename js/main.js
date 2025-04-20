eb6e798b0879eed822a6d1e71ee58f2b75ede7e3
Francisco Alejandro Padilla Gomez <padill77@msu.edu>
2025-04-20 12:23:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // Mobile nav toggle
-document.getElementById('navToggle').addEventListener('click', () => {
+document.getElementById('navToggle').addEventListener('click',()=>{
   document.getElementById('nav').classList.toggle('open');
 });
